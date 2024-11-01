cc050a093446a47c51fec53d5683cf52d7de6a96
datarec <73265375+datarec@users.noreply.github.com>
2024-11-01 19:24:14 +1100

    Update main.c

--- main.c
+++ main.c
@@ -2,25 +2,47 @@
 
 #include <stdio.h>
 
+ struct student {
+    char studentFname;
+    char studentLname;
+
+  };
+
 
 void addStudent() {
-  printf("In student");
+  // add students function.
+  struct student {
+    char studentFname;
+    char studentLname;
+    char studentDob;
+  };
+  
+  struct student studentInfo; 
+  FILE* students;
+  students = fopen("students.txt", "w");
+  printf("\nPlease enter the students information.\n\n");
+  printf("First name (Name at birth) ");
+  scanf("%s", &studentInfo.studentFname);
+  printf("Student last name ");
+  scanf("%s", &studentInfo.studentLname);
+  printf("Student DOB (dd/mm/yyyy) ");
+  scanf("%s", &studentInfo.studentDob);
 }
 
 
 void searchStudent() {
-  printf("In search");
+  printf("Search student.");
 }
 
 
 void smsMain() {
   int smsMainOption;
   printf("Welcome to the school's Student Management System.");
+  printf("\nStudents enrolled: 0");
   printf("\n\n1) Add a student");
-  printf("\n2) Search student (using role-call number)");
-  printf("\n\n>> ");
-  scanf("%d", smsMainOption); 
-  printf("UI chjeck from thingp. /%d", smsMainOption);
+  printf("\n2) Search student (ADD MORE OPTIONS. using role-call number)");
+  printf("\n\n[>] ");
+  scanf("%d", &smsMainOption); 
   if (smsMainOption == 1) {
     addStudent();
   } 
